792c7c8b4eba329b86aadf7a29b83a22b6021707
Aruna Fernando <aruna.lakmal2@inter.ikea.com>
2025-08-08 15:26:48 +0200

    Test Deletion

--- scripts/clone-db.sh
+++ scripts/clone-db.sh
@@ -70,7 +70,7 @@ delete_clone() {
     else
         log "INFO" "Executing database deletion process..."
         log "INFO" "Please wait! this process will take a while..."
-
+        construct_suffix
         az sql db delete --name ${SOURCE_DB_NAME}-${SUFFIX} \
         --resource-group ${RESOURCE_GROUP} \
         --server ${SERVER_NAME} --yes > /dev/null 2>&1
